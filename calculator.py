47f255446cee26875e80baece36aec82c4d72831
RohanshresthaGit <150516010+RohanshresthaGit@users.noreply.github.com>
2025-08-18 22:12:07 +0545

    create calculator and form

---
+++ calculator.py
@@ -0,0 +1,52 @@
+def add(num1, num2):
+    return num1 + num2
+    
+def sub(num1, num2):
+    return num1 - num2
+    
+def multiply(num1, num2):
+    return num1 * num2
+    
+def divide(num1, num2):
+    if(num2 == 0):
+        return "Cant divide by 0."
+    return num1 / num2
+    
+def modulo(num1, num2):
+    return num1 % num2
+    
+def power(num1, num2):
+    return num1 ** num2
+    
+def floorDivision(num1, num2):
+    if(num2 == 0):
+        return "Cant divide by 0."
+    return num1 // num2
+    
+num1 = int(input("Please Enter your 1st number:\t"))
+num2 = int(input("Please Enter your 2st number:\t"))
+op = input("\nPlease enter the operator to perform calculation\nfor eg.\n+ for addition\n- for subtraction\n* for multiplication\n/ for division\n** for exponensiation\n// for floor division\noperator:\t")
+
+if(op == "+"):
+    print(f"{num1}  {op}  {num2} = {add(num1, num2)}")
+elif(op == "-"):
+    print(f"{num1}  {op}  {num2} = {sub(num1, num2)}")
+elif(op == "*"):
+    print(f"{num1}  {op}  {num2} = {multiply(num1, num2)}")
+elif(op == "/"):
+    if(num2 == 0):
+        print(divide(num1, num2))
+    else:
+        print(f"{num1}  {op}  {num2} = {divide(num1, num2)}")
+elif(op == "%"):
+    print(f"{num1}  {op}  {num2} = {modulo(num1, num2)}")
+elif(op == ""):
+    print(f"{num1}  {op}  {num2} = {power(num1, num2)}")
+elif(op == "//"):
+    if(num2 == 0):
+        print(floorDivision(num1, num2))
+    else:
+        print(f"{num1}  {op}  {num2} = {floorDivision(num1, num2)}")
+else:
+    #Error message if user enters an invalid operator
+    print("Enter a valid operator")
---
+++ form.py
@@ -0,0 +1,31 @@
+### function to get nickname
+### handles all edge cases for NickName
+### works fine if user enters one word or multiple word for name.
+def getNickname(name: str) -> str:
+    parts = name.split()
+    if len(parts) == 1:
+        return parts[0]
+    else:
+        return parts[0][:2] + parts[-1][-2:]
+
+
+name = input("Please Enter your Full name:\t").strip()
+age = input("Enter your age:\t").strip()
+height_input = input("Enter your height in feet:\t").strip()
+country = input("Enter your country name:\t").strip()
+
+# Check if any data is missing
+if not name or not age or not height_input or not country:
+    print("\nPlease enter all required data!")
+else:
+    try:
+        # Parsing height to float.
+        height = float(height_input) 
+        print(f"\nHello {name.upper()}")
+        print(f"You are {age} years old.")
+        print(f"Your height is {round(height, 2)} feet.")
+        print(f"You are from {country.capitalize()}.")
+        print(f"Your Nickname is {getNickname(name).upper()}")
+    except ValueError:
+        # print the error message if the type conversion fails.
+        print("\nHeight must be a valid number!")
